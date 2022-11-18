79b5bbea38ce0a3cc00905eecb6ef69c6e0ac970
MARKUSA19 <90573374+MARKUSA19@users.noreply.github.com>
2022-11-18 16:14:17 +0100

    Update wypozyczsubmit.php

--- wypozyczsubmit.php
+++ wypozyczsubmit.php
@@ -11,7 +11,7 @@ $Klasa=$_GET['klasa'];
 $D_oddania=$_GET['d_oddania'];
 $LP=$_GET['lp'];
 
-$con = new mysqli("localhost","id19715355_root","E1Nc&M@>I=@?Sw]~","id19715355_baza");
+$con = new mysqli("localhost","root","","baza");
 $q = "INSERT INTO zamowienia (Imie, Nazwisko, LP_ksiazki, Klasa, Data_oddania) VALUES ('".$Imie."' , '".$Nazwisko."', '".$LP."', '".$Klasa."', '".$D_oddania."'); ";
 $r = mysqli_query($con, $q);
 header( "Location: index.html?success=1" ); 
